b6ff3b9d88791bc894edfa6f7b4f0f185bfef271
Lada <lpe7702@gmail.com>
2021-04-16 00:25:00 +0200

    Hirdetesek adatbazis szerkezete

--- Aprohirdetes/src/main/java/Model/Hirdetesek.java
+++ Aprohirdetes/src/main/java/Model/Hirdetesek.java
@@ -1,18 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
-/**
- *
- * @author lpe77
- */
-public class Hirdetesek {
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-    public Hirdetesek() {
-    }
+@Entity
+@Table(name = "hirdetesek")
+public class Hirdetesek 
+{
+    @Id
+    @GeneratedValue()
+    int id;
     
+    @Column(nullable = false)
+    String nev;
+    
+    @Column(nullable = false)
+    String leiras;
+    
+    @Column(nullable = false)
+    LocalDate date;  
+    
+    @Column(nullable = false)
+    String hely;
+    
+    @Enumerated(EnumType.STRING)
+    Csomagkuldes csomagkuldes;
+    
+    @Column(nullable = false)
+    int ar;
+
+    public enum Csomagkuldes {
+        IGEN, NEM;
+    }
     
 }
