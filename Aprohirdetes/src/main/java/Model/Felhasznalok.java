b2a9c492bebf3c21e5b3a0066acf4dbb55b8cb9f
Lada <lpe7702@gmail.com>
2021-04-16 00:20:56 +0200

    Hirdetesek Model

--- Aprohirdetes/src/main/java/Model/Felhasznalok.java
+++ Aprohirdetes/src/main/java/Model/Felhasznalok.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Model;
 
-/**
- *
- * @author lpe77
- */
-public class Felhasznalok {
-    
-    
+public class Felhasznalok 
+{
+
 }
