32865f12d3682e6a948bab962ba53a818aec3528
szanyidani2000 <szanyi.dani2000@gmail.com>
2021-04-17 13:43:38 +0200

    Navbar frontend

--- Aprohirdetes/src/main/java/aprohirdetes/MainApp.java
+++ Aprohirdetes/src/main/java/aprohirdetes/MainApp.java
@@ -1,12 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aprohirdetes;
-
-import Controller.FXMLMainSceneController;
-import Model.Hirdetesek;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
@@ -14,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class MainApp extends Application{
 
@@ -22,13 +15,15 @@ public class MainApp extends Application{
         Parent root = FXMLLoader.load(getClass().getResource("/FXML/FXMLMainScene.fxml"));
         
         Scene scene = new Scene(root);
+        scene.getStylesheets().add("/Style/CSSMainScene.css");      
         
         stage.setTitle("Apróhirdetés");
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
-        
-        
     }
+    
     public static void main(String[] args) {
         launch(args);
     }
