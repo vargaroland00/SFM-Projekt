c9ed650c6c041fc6d281b04b3cc3377221b78f30
szanyidani200 <szanyi.dani2000@gmail.com>
2021-04-18 01:48:47 +0200

    Szerver indítása applikációval együtt

--- Aprohirdetes/src/main/java/aprohirdetes/MainApp.java
+++ Aprohirdetes/src/main/java/aprohirdetes/MainApp.java
@@ -1,17 +1,19 @@
 package aprohirdetes;
 
+import java.sql.SQLException;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.h2.tools.Server;
 
 public class MainApp extends Application{
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) throws Exception 
+    {
         Parent root = FXMLLoader.load(getClass().getResource("/FXML/FXMLMainScene.fxml"));
         
         Scene scene = new Scene(root);
@@ -25,7 +27,18 @@ public class MainApp extends Application{
     }
     
     public static void main(String[] args) {
+        try {
+            startDatabase();
+            System.out.println("Running...");
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+        
         launch(args);
     }
     
+    
+    private static void startDatabase() throws SQLException {
+        new Server().runTool("-tcp", "-web", "-ifNotExists");
+    }
 }
