e9a42ba5af85c0dc59ebc97064dc0a191aa7f4d9
Roxor456 <lpe7702@gmail.com>
2021-05-17 02:10:22 +0200

    Ár oszlop hozzáadása, név oszlop formázása

--- Aprohirdetes/src/main/java/Controller/FXMLMainSceneController.java
+++ Aprohirdetes/src/main/java/Controller/FXMLMainSceneController.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,11 +17,16 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import javafx.util.Callback;
 
 public class FXMLMainSceneController implements Initializable {
     
@@ -32,6 +38,9 @@ public class FXMLMainSceneController implements Initializable {
      
     @FXML
     private TableColumn<Hirdetesek, String> nevHirdetesekColumn;
+    
+    @FXML
+    private TableColumn<Hirdetesek, String> arHirdetesekColumn;
 
     @FXML
     private TableColumn<Hirdetesek, String> helyHirdetesekColumn;
@@ -72,40 +81,28 @@ public class FXMLMainSceneController implements Initializable {
             hirdetesekTableData.addAll(hirdetesekDataQuery);
             
             nevHirdetesekColumn.setCellValueFactory(new PropertyValueFactory<>("nev"));
+            
+            nevHirdetesekColumn.setCellFactory(tableColumn -> 
+            {
+                TableCell<Hirdetesek, String> tableCell = new TableCell<>();
+                
+                Text text = new Text();
+                text.setTextAlignment(TextAlignment.CENTER);
+                
+                tableCell.setGraphic(text);
+                tableCell.setPrefHeight(Control.USE_COMPUTED_SIZE);
+                
+                //text.wrappingWidthProperty().bind(nevHirdetesekColumn.widthProperty());
+                text.setWrappingWidth(nevHirdetesekColumn.getWidth() - 20);
+                
+                text.textProperty().bind(tableCell.itemProperty());
+                
+                return tableCell;
+            });
+            
+            arHirdetesekColumn.setCellValueFactory(new PropertyValueFactory<>("ar"));
             helyHirdetesekColumn.setCellValueFactory(new PropertyValueFactory<>("hely"));
             feladasidejeHirdetesekColumn.setCellValueFactory(new PropertyValueFactory<>("feladasideje"));
-            
-            /*nevHirdetesekColumn.setCellFactory(new Callback<TableColumn<Hirdetesek,String>, TableCell<Hirdetesek,String>>() {
-			@Override
-			public TableCell<Hirdetesek, String> call( TableColumn<Hirdetesek, String> param) {
-				final TableCell<Hirdetesek, String> cell = new TableCell<Hirdetesek, String>() {
-					private Text text;
-					@Override
-					public void updateItem(String item, boolean empty) {
-						super.updateItem(item, empty);
-						//if (!isEmpty()) {
-							text = new Text(item.toString());
-							text.setWrappingWidth(400); // Setting the wrapping width to the Text
-							setGraphic(text);
-						//}
-					}
-                                        
-                                        
-				};
-				return cell;
-			}
-                        
-                        /*@Override
-                        public TableCell<Hirdetesek, String> call( TableColumn<Hirdetesek, String> param) {
-                            TableCell<Hirdetesek, String> cell = new TableCell<>();
-                            Text text = new Text();
-                            cell.setGraphic(text);
-                            //text.wrappingWidthProperty().bind(cell.widthProperty());
-                            text.setWrappingWidth(400); // Setting the wrapping width to the Text
-                            //text.textProperty().bind(cell.itemProperty());
-                            return cell ;
-                        }
-                        });*/
             
             hirdetesekTable.setItems(hirdetesekTableData);
         } 
