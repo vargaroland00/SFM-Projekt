9dcff6182fef0a6360f781e8ca7ddabbfd456b47
Roxor456 <lpe7702@gmail.com>
2021-05-18 03:40:19 +0200

    Bejelentkezes helyes kitoltes (blank, regex)

--- Aprohirdetes/src/main/java/Controller/FXMLLoginSceneController.java
+++ Aprohirdetes/src/main/java/Controller/FXMLLoginSceneController.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -16,13 +18,24 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.stage.Window;
 
 public class FXMLLoginSceneController implements Initializable 
 {
     @FXML
+    private TextField emailBejelentkezesTextbox;
+
+    @FXML
     private Button LoginButton;
+
+    @FXML
+    private TextField emailRegisztracioTextbox;
+
+    @FXML
+    private TextField nevTextbox;
 
     @FXML
     private Button RegisztracioButton;
@@ -44,23 +57,60 @@ public class FXMLLoginSceneController implements Initializable
 
     @FXML
     private Label nevRegisztracioWarningLabel;
-    
+
+    @FXML
+    private PasswordField jelszoBejelentkezesTextbox;
+
+    @FXML
+    private PasswordField jelszoRegisztracioTextbox;
+
+    @FXML
+    private PasswordField megerositoJelszoTextbox;
+
     @FXML
     private ChoiceBox<String> jogosultsagDropdown;
+
+    @FXML
+    private TextField telefonszamTextbox;
+
+    @FXML
+    private Label telefonszamRegisztracioWarningLabel;
     
     private ObservableList<String> jogosultsagList = FXCollections.observableArrayList("Felhasznalo", "Admin");
     
+    @Override
+    public void initialize(URL url, ResourceBundle rb) 
+    {
+        jogosultsagDropdown.setValue(jogosultsagList.get(0));
+        jogosultsagDropdown.setItems(jogosultsagList);
+        
+        bejelentkezesWarningInitialize();
+        regisztracioWarningInitialize();
+    }
+    
+    private void bejelentkezesWarningInitialize() 
+    {
+        emailBejelentkezesWarningLabel.setVisible(false);
+        jelszoBejelentkezesWarningLabel.setVisible(false);
+    }
+    
+    private void regisztracioWarningInitialize()
+    {
+        emailRegisztracioWarningLabel.setVisible(false);
+        jelszoRegisztracioWarningLabel.setVisible(false);
+        megerositojelszoRegisztracioWarningLabel.setVisible(false);
+        nevRegisztracioWarningLabel.setVisible(false);
+        telefonszamRegisztracioWarningLabel.setVisible(false);
+    }
+    
     @FXML
-    private Label telefonszamRegisztracioWarningLabel;
-
-    @FXML
-    void onRegisztracioButton() 
+    private void onRegisztracioButton() 
     {
         
     }
     
     @FXML
-    void onLoginButton() 
+    private void onLoginButton() 
     {
         try 
         {
@@ -81,28 +131,46 @@ public class FXMLLoginSceneController implements Initializable
         }
     }
     
-    @Override
-    public void initialize(URL url, ResourceBundle rb) 
+    private boolean bejelentkezesKitoltesEllenorzes() 
     {
-        jogosultsagDropdown.setValue(jogosultsagList.get(0));
-        jogosultsagDropdown.setItems(jogosultsagList);
+        boolean mindenHelyesenKitoltve = true;
         
-        warningInitialize();
+        String emailRegex = "/^(([^<>()[\\]\\\\.,;:\\s@\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$/";
+        Pattern emailPattern = Pattern.compile(emailRegex);
+        Matcher emailMatcher = emailPattern.matcher(emailBejelentkezesTextbox.getText());
+        
+        if (emailBejelentkezesTextbox.getText().isBlank())
+        {
+            emailBejelentkezesWarningLabel.setVisible(true);
+            emailBejelentkezesWarningLabel.setText("Nem adta meg az email címet!");
+            mindenHelyesenKitoltve = false;
+        }
+        else if (!emailMatcher.matches())
+        {
+            emailBejelentkezesWarningLabel.setVisible(true);
+            emailBejelentkezesWarningLabel.setText("Hibás email cím formátum!");
+            mindenHelyesenKitoltve = false;
+        }
+        
+        if (jelszoBejelentkezesTextbox.getText().isBlank())
+        {
+            jelszoBejelentkezesWarningLabel.setVisible(true);
+            jelszoBejelentkezesWarningLabel.setText("Nem adta meg a jelszót!");
+            mindenHelyesenKitoltve = false;
+        }
+        
+        return mindenHelyesenKitoltve;
     }
     
-    public void warningInitialize() 
+    private boolean regisztracioKitoltesEllenorzes() 
     {
-        emailBejelentkezesWarningLabel.setVisible(false);
-        jelszoBejelentkezesWarningLabel.setVisible(false);
-        emailRegisztracioWarningLabel.setVisible(false);
-        jelszoRegisztracioWarningLabel.setVisible(false);
-        megerositojelszoRegisztracioWarningLabel.setVisible(false);
-        nevRegisztracioWarningLabel.setVisible(false);
-        telefonszamRegisztracioWarningLabel.setVisible(false);
+        boolean mindenHelyesenKitoltve = true;
+        
+        return mindenHelyesenKitoltve;
     }
     
     @FXML
-    void onKilepesButton() {
+    private void onKilepesButton() {
         Platform.exit();
     }
 }
