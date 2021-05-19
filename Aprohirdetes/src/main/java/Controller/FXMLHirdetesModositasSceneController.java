d47d2cd97fd08d531704f038e385cdf50c3315c0
Roxor456 <lpe7702@gmail.com>
2021-05-19 15:28:05 +0200

    HirdetesModositas -> Atiranyitas ha mégsem

--- Aprohirdetes/src/main/java/Controller/FXMLHirdetesModositasSceneController.java
+++ Aprohirdetes/src/main/java/Controller/FXMLHirdetesModositasSceneController.java
@@ -1,15 +1,21 @@
 package Controller;
 
 import Model.Hirdetesek;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 
 public class FXMLHirdetesModositasSceneController implements Initializable
 {
@@ -55,9 +61,28 @@ public class FXMLHirdetesModositasSceneController implements Initializable
     void onHirdetesFeladasaButton() {
 
     }
+    
+    void atiranyitasFoablak()
+    {
+        try {
+        Parent mainSceneRoot = FXMLLoader.load(getClass().getResource("/FXML/FXMLMainScene.fxml"));
+        
+        Scene hirdetesScene = hirdetesFeladasaButton.getScene();
+        Window window = hirdetesScene.getWindow();
+        
+        Stage MainStage = (Stage) window;
+        hirdetesFeladasaButton.getScene().setRoot(mainSceneRoot);
+        
+        MainStage.setTitle("Hirdetés feladás");
+        MainStage.show();
+        
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
     @FXML
     void onMegsemButton() {
-
+        atiranyitasFoablak();
     }
 }
